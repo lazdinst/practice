a1c0754d6815e2be55a1a20b1f1711a53cec56b9
lazdinst <lazdinst@gmail.com>
2020-06-23 08:58:29 -0700

    single number completed

---
+++ singleNumberII.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = nums => {
+  const hash = new Map();
+  let single = null;
+
+  for(let i = 0; i < nums.length; i++) {
+    let count = 0;
+    if(hash.has(nums[i])) {
+      count = hash.get(nums[i]) + 1;
+    } else {
+      count ++;
+    }
+    hash.set(nums[i], count);
+    
+  }
+
+  hash.forEach((value, key, index) => {
+    if(value === 1) {
+      single = key;
+    }
+  })
+
+  return single;
+};
+
+const nums = [2,2,3,4,5,4,5];
+console.log(singleNumber(nums));
