a0cfbe7963937e46cdc134fae36c84c278224a9c
lazdinst <lazdinst@gmail.com>
2021-02-08 20:27:13 -0800

    optimal done

--- maxArea.js
+++ maxArea.js
@@ -43,12 +43,43 @@ var maxArea = (height) => {
   return max;
 };
 
+var maxAreaOptimal = h => {
+  if(!h.length) return 0;
+
+  let left = 0, right = h.length - 1;
+  let maxArea = 0;
+  let areas = [];
+
+  while(left < right) {
+    let length = right - left;
+    let height = Math.min(h[left], h[right]);
+
+    let area = length * height;
+
+    areas.push(area);
+
+    if(maxArea < area) {
+      maxArea = area;
+    }
+
+    if(h[left] < h[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea
+}
+
 // |       |
 // | |     |
 // | | |   |
 // | | | | |
 
 let h = [4,3,2,1,4];
-let r = maxArea(h);
+let h2 = [1,8,6,2,5,4,8,3,7];
+let r = maxAreaOptimal(h);
 
 console.log(r);
+console.log(maxAreaOptimal(h2))
